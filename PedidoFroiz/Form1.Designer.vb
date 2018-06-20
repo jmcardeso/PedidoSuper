2be3a3cfd7841f445542b584ef5465ab4b7f9cca
Juan Manuel Cardeso García <jmcardeso@outlook.es>
2018-06-20 18:28:27 +0200

    Fase alfa muy básica

---
+++ PedidoFroiz/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPedidoFroiz
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.tbxResultado = New System.Windows.Forms.TextBox()
+        Me.tbxProducto = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(145, 102)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(101, 48)
+        Me.btnBuscar.TabIndex = 0
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'tbxResultado
+        '
+        Me.tbxResultado.Location = New System.Drawing.Point(12, 198)
+        Me.tbxResultado.Multiline = True
+        Me.tbxResultado.Name = "tbxResultado"
+        Me.tbxResultado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbxResultado.Size = New System.Drawing.Size(373, 240)
+        Me.tbxResultado.TabIndex = 1
+        '
+        'tbxProducto
+        '
+        Me.tbxProducto.Location = New System.Drawing.Point(10, 55)
+        Me.tbxProducto.Name = "tbxProducto"
+        Me.tbxProducto.Size = New System.Drawing.Size(375, 20)
+        Me.tbxProducto.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Producto:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 182)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Resultado:"
+        '
+        'frmPedidoFroiz
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(398, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tbxProducto)
+        Me.Controls.Add(Me.tbxResultado)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Name = "frmPedidoFroiz"
+        Me.Text = "PedidoFroiz"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents tbxResultado As TextBox
+    Friend WithEvents tbxProducto As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ PedidoFroiz/Form1.vb
@@ -0,0 +1,102 @@
+﻿Imports System.IO
+Imports System.Xml
+
+Public Class frmPedidoFroiz
+#Region "Constantes"
+    Const IDENTIFICADOR_PRECIO_INICIO = "data-price="
+    Const IDENTIFICADOR_PRECIO_FINAL = Chr(34)
+    Const IDENTIFICADOR_NOMBRE_INICIO = "dproducto " & Chr(34) & ">"
+    Const IDENTIFICADOR_NOMBRE_FINAL = "  "
+    Const URL_BUSQUEDA = "https://www.froiz.com/shop/search.php?q="
+#End Region
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Dim sResultadoHTML As String = ""
+        Dim Productos As New List(Of Producto)
+        tbxResultado.Text = ""
+
+        If tbxProducto.Text <> "" Then
+            sResultadoHTML = RetHTML(URL_BUSQUEDA & tbxProducto.Text)
+            Productos = RetProductos(sResultadoHTML)
+            CrearXMLProductos(Productos)
+            For Each Producto In Productos
+                tbxResultado.Text &= Producto.Nombre & " " & Producto.Precio.ToString & "€" & vbCrLf
+            Next
+        End If
+
+    End Sub
+#Region "Funciones auxiliares"
+    ''' <summary>
+    ''' Devuelve una cadena con el contenido de una página web
+    ''' </summary>
+    ''' <param name="Url">La dirección de la página web (incluyendo http://)</param>
+    ''' <returns>La cadena con el volcado de la página web</returns>
+    Public Function RetHTML(ByVal Url As String) As String
+        Dim WR As System.Net.WebRequest
+        Dim Rsp As System.Net.WebResponse
+        WR = System.Net.WebRequest.Create(Url)
+        Rsp = WR.GetResponse()
+        Return New StreamReader(Rsp.GetResponseStream()).ReadToEnd()
+    End Function
+
+    ''' <summary>
+    ''' Devuelve una lista de productos contenidos en una cadena volcada de una página web
+    ''' </summary>
+    ''' <param name="HTML">La cadena con la página web volcada</param>
+    ''' <returns>La lista de productos, con su nombre y su precio</returns>
+    Public Function RetProductos(ByVal HTML As String) As List(Of Producto)
+        Dim listaProductos As New List(Of Producto)
+        Dim IndexINI As Integer = 0
+        Dim IndexFIN As Integer = 0
+        Dim sPrecio As String
+        Dim Precio As Decimal
+        Dim sNombre As String
+
+        Do
+            IndexINI = HTML.IndexOf(IDENTIFICADOR_PRECIO_INICIO, IndexINI) 'Miramos la posición de la primera aparición del precio
+            If IndexINI < 0 Then 'Si el índice es -1 significa que no se encontró nada y salimos
+                Exit Do
+            Else
+                IndexINI += IDENTIFICADOR_PRECIO_INICIO.Length + 1 'Si encuentra un precio suma la longitud del identificador
+                'a la posición para quedar en el sitio justo
+            End If
+
+            IndexFIN = HTML.IndexOf(IDENTIFICADOR_PRECIO_FINAL, IndexINI) 'Buscamos la posición final del precio
+            sPrecio = HTML.Substring(IndexINI, IndexFIN - IndexINI) 'Generamos la subcadena con las posiciones correctas
+            Precio = CDec(sPrecio.Replace(".", ",")) 'Convertimos en decimal la cadena y sustituímos el punto por la coma decimal
+
+            IndexINI = HTML.IndexOf(IDENTIFICADOR_NOMBRE_INICIO, IndexFIN) + IDENTIFICADOR_NOMBRE_INICIO.Length 'Lo mismo con el nombre del producto
+            IndexFIN = HTML.IndexOf(IDENTIFICADOR_NOMBRE_FINAL, IndexINI)
+            sNombre = HTML.Substring(IndexINI, IndexFIN - IndexINI)
+
+            listaProductos.Add(New Producto() With {.Nombre = sNombre, .Precio = Precio}) 'Añadimos el producto a la lista
+        Loop
+
+        Return listaProductos
+
+    End Function
+
+    ''' <summary>
+    ''' Crea un fichero xml con la lista de productos
+    ''' </summary>
+    ''' <param name="listaProductos">La lista de productos de la que se va a crear el fichero</param>
+    Public Sub CrearXMLProductos(ByVal listaProductos As List(Of Producto))
+        Dim Documento As New XElement("Productos")
+
+        For Each Producto In listaProductos
+            Documento.Add(New XElement("Producto", New XElement("Nombre", Producto.Nombre), New XElement("Precio", Producto.Precio)))
+        Next
+
+        Documento.Save(Application.StartupPath & "\Productos.xml")
+    End Sub
+
+#End Region
+End Class
+
+''' <summary>
+''' La clase Producto contiene el nombre y el precio de los productos
+''' </summary>
+Public Class Producto
+    Public Property Nombre As String
+    Public Property Precio As Decimal
+End Class
